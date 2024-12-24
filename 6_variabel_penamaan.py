24a81e6717add23f84138a2a5e4c6c078f725b38
zauadinata <natazauhair@gmail.com>
2024-12-24 19:50:44 +0700

    Add files via upload

---
+++ 6_variabel_penamaan.py
@@ -0,0 +1,20 @@
+# variabel biasa digunakan untuk mendeklarasikan suatu data
+
+# penggunaan variabel
+nama = 'adinata' # kata 'nama' merupakan variabel yang memiliki data yaitu 'adinata'
+print (nama) # mencetak isi variabel dengan memanggil variabelnya yaitu 'nama'
+
+
+# penggunaan variabel dalam operasi aritmatika
+angka_1 = 20
+angka_2 = 50
+
+hasil = angka_1 + angka_2 #membuat variabel 'hasil untuk menambahkan variabel 'angka_1' dan 'angka_2'
+print (hasil) # mencetak hasil dari penjumlahan dengan memanggil variabel 'hasil'
+
+# manipulasi variabel
+# jika nama variabel memiliki 2 nama yang sama, maka program akan menjalankan variabel yang terakhir karena adanya pembaharuan
+angka = 1
+angka = 2
+print (angka) # akan menampilkan '2'
+
