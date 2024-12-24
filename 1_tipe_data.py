3f691d86b82e982ba7a216b0181560a44986307d
zauadinata <natazauhair@gmail.com>
2024-12-24 19:47:16 +0700

    Add files via upload

---
+++ 1_tipe_data.py
@@ -0,0 +1,17 @@
+# mengetahui jenis tipe data dari dalam variabel
+
+nama = 'adinata' # variabel nama yang memiliki data 'adinata'
+print (nama) # mencetak data dari variabel
+print (type (nama)) # mengetahui tipe data dari variabel 'nama'
+
+angka = 20
+print (angka)
+print (type(angka))
+
+angka_1 = 20.55
+print (angka_1)
+print (type (angka_1))
+
+boolean = True
+print (boolean)
+print (type (boolean))
