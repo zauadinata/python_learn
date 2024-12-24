be2707e465fdf5ca0ba8603d8835d878d7c5bcb7
zauadinata <natazauhair@gmail.com>
2024-12-24 19:47:54 +0700

    Add files via upload

---
+++ 2_konversi_tipe_data.py
@@ -0,0 +1,16 @@
+# data awal
+data_angka = 10 # deklarasi dengan variabel
+print ('data ini adalah : ', data_angka) # mencetak data dari variabel
+print ('data bertipe    : ', (type (data_angka))) # mengetahui jenis tipe data dari variabel 'data_angka'
+
+# konversi data menjadi format lain
+# sebagai contoh dikonversi menjadi tipe data float
+
+# membuat variabel baru yaitu 'data_konversi'
+# memanggil tipe data float dan variabel sebelumnya yang bertipe integer (int)
+data_konversi = float (data_angka)
+
+print ('data ini adalah : ', data_konversi) # mencetak data baru
+print ('data bertipe    : ', (type (data_konversi))) #mengetahui jenis tipe data dari variabel 'data_konversi'
+
+# penggunaan konversi juga dapat dilakukan sebaliknya
