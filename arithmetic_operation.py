837fdcfc8c90c0173604b02f1a1b8208c7c800d0
Muhammad Shafa Zauhair Adinata <natazauhair@gmail.com>
2025-01-09 17:32:52 +0700

    Add files via upload

---
+++ arithmetic_operation.py
@@ -0,0 +1,31 @@
+'''
+sum         : +         -> tambah
+difference  : -         -> kurang
+product     : *         -> kali
+quotient    : /         -> bagi
+mod         : %         -> sisa bagi atau modulus
+power       : **        -> pangkat
+integer div : //        -> bagi dengan hasil bil. bulat
+'''
+
+# sum
+a = 5 
+b = 5
+hasil_tambah = a + b
+hasil_kurang = a - b
+hasil_kali =  a * b
+hasil_bagi = a / b
+hasil_modulus = a % b
+hasil_power = a ** b
+hasil_div = a // b
+
+print ('hasil tambah        : ', hasil_tambah)
+print ('hasil kurang        : ', hasil_kurang)
+print ('hasil kali          : ', hasil_kali)
+print ('hasil bagi          : ', hasil_bagi)
+print ('hasil modulus       : ', hasil_modulus)
+print ('hasil pangkat       : ', hasil_power)
+print ('hasil div           : ', hasil_div)
+
+
+
